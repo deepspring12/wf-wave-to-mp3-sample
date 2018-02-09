26610525c62f5170999680b4572fdba5cd881476
deepspring <deepspring@yahoo.com>
2018-02-09 12:14:15 +0530

    Small changes. Added Comments.

--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,26 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 
+/*WaveToMp3ConverterWF is a simple app that converts a wave file to mp3 file using LAME ENCODER ENGINE.
+ * Lame Source Code: http://lame.sourceforge.net/
+ *  
+ *  In the background, 
+ *  1) app runs process cmd.exe
+ *  2) passes a command String to lame.exe based on user input, in the following format
+ *      
+ *      lame cmdArgs inputfilename.wav outputfilename.mp3
+ *  
+ *  3) displays the command that runs in a textbox.
+ *  
+ *  Supports basic parameters for convertsion
+ *   - VBR/CBR (Variable Bitrate or Constant Bitrate)
+ *   - ABR (Average Bitrate)
+ *   - mono encoding
+ *   
+ *   Compilating lame.exe on windows
+ *   lame.exe can be compiled from source code using VS or nasm.exe
+ *   See more at:https://wehuberconsultingllc.com/wordpress/2007/12/30/building-the-lame-mp3-encoder-using-visual-studio-8-express/
+*/
 
 namespace WindowsFormsApp1
 {
@@ -20,15 +40,18 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        //global variables    
         string inputFilePath;
         string outputFilePath;
         string fileName;
-        string command = "/C lame ";
+        string command = "/C lame "; // use /K lame for testing
         string cmdArgs = "";
         string strCmdText = "";
 
         Boolean bitrateMode = false;//by default bitrate radiobutton is unchecked
         Boolean qualityMode = true;//default quality radiobutton is checked
+
         Boolean monoEncoding = false; //by default monoencoding is unchecked
         Boolean CBR_checkbox = false; //by default CBR is unchecked
 
@@ -36,12 +59,15 @@ namespace WindowsFormsApp1
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                //get input filepaths and filenames 
                 inputFilePath = openFileDialog1.FileName;
                 fileName = Path.GetFileNameWithoutExtension(inputFilePath);
                 textBox1.Text = inputFilePath;
+                //set output filepath and filename same as source (filename & directoryPath)
                 outputFilePath = Path.GetDirectoryName(inputFilePath) + "\\" + fileName + ".mp3";
                 textBox2.Text = outputFilePath;
 
+                //Enable all ui components
                 outputBtn.Enabled = true;
                 textBox2.Enabled = true;
                 TargetGroupBox.Enabled = true;
@@ -58,8 +84,10 @@ namespace WindowsFormsApp1
 
         private void outputBtn_Click(object sender, EventArgs e)
         {
+            //if output folder chosen by user
             if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
+                //set filename and extension same as source filename
                 outputFilePath = folderBrowserDialog1.SelectedPath + "\\" + fileName +".mp3";
                 textBox2.Text = outputFilePath;
             }
@@ -76,9 +104,11 @@ namespace WindowsFormsApp1
             else
             {
                 setCommandArguments();
-
+                
+                //start background thread 
                 Thread LameProcessThread = new Thread(runLameCommand);
                 LameProcessThread.Start();
+
                 tsProgressBar.Visible = true;
                 tsProgressBar.Style = ProgressBarStyle.Marquee;
                 EncodeBtn.Enabled = false;   
@@ -104,21 +134,34 @@ namespace WindowsFormsApp1
           
             if (process.HasExited)
             {
+                //if no error, update statusbar, progessbar, show success dialog
                 tsProgressBar.Style = ProgressBarStyle.Continuous;
                 tsProgressBar.Value = tsProgressBar.Maximum;
                 toolStripStatusLabel1.Text = "Done!";
                 DialogResult res = MessageBox.Show("File Encoded Succesfully!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.None);
-                tsProgressBar.Visible = false;
-                command = "/C lame ";
-                cmdArgs = "";
-                strCmdText = "";
-                toolStripStatusLabel1.Text = "Select Another File...";
-                tb.Text = "";
-                EncodeBtn.Enabled = true;
+
+                //to implement get error message
+
+                ResetApp();
+
                 return;
                 
             }
 
+        }
+        private void ResetApp()
+        {
+            //reset variables
+            command = "/C lame ";
+            cmdArgs = "";
+            strCmdText = "";
+
+            //reset ui components
+            EncodeBtn.Enabled = true;
+            tsProgressBar.Visible = false;
+            toolStripStatusLabel1.Text = "Select Another File...";
+
+            tb.Text = "";
         }
 
         private void setCommandArguments()
@@ -176,11 +219,13 @@ namespace WindowsFormsApp1
             }
             cmdArgs += priority;
 
-            strCmdText = command + cmdArgs + " "+ inputFilePath + " " + outputFilePath;
+            strCmdText = command + cmdArgs + " " + inputFilePath + " " + outputFilePath;
             tb.Text = strCmdText;
 
         }
 
+
+        //Track changes of UI Components 
         private void BitrateRadioButton_CheckedChanged(object sender, EventArgs e)
         {
             if (BitrateRadioButton.Checked == true)
@@ -223,6 +268,7 @@ namespace WindowsFormsApp1
 
         private void QualityTrackBar_Scroll(object sender, EventArgs e)
         {
+            
             QualityNumericUpDown.Value = QualityTrackBar.Value;
 
         }
@@ -240,6 +286,11 @@ namespace WindowsFormsApp1
 
         private void QualityNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
+            //Lame uses a range [9.999 to 0] lowest quality to highest quality
+            // Lowest Value 99,  Highest Value 0
+            //changing the scale:
+            //mapping float [9.999,0] to int [99,0]
+            
             QualityTrackBar.Value = (int)(QualityNumericUpDown.Value);
         }
 
